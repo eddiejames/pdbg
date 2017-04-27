f69c98e3660160d1e9ee804a28cde4740d9e266a
Edward A. James <eajames@us.ibm.com>
2017-04-27 16:35:50 -0500

    Add support for new FSI paths and endianness swap
    4.10 Kernel switched FSI behavior.
    
    Signed-off-by: Edward A. James <eajames@us.ibm.com>

--- libpdbg/kernel.c
+++ libpdbg/kernel.c
@@ -21,6 +21,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <assert.h>
+#include <endian.h>
 #include <errno.h>
 #include <err.h>
 #include <inttypes.h>
@@ -29,12 +30,28 @@
 #include "operations.h"
 #include "target.h"
 
-#define FSI_SCAN_PATH "/sys/devices/platform/fsi-master/scan"
-#define FSI_CFAM_PATH "/sys/devices/platform/fsi-master/slave@00:00/raw"
+#define NUM_TRIES_PROBE	5
+#define FSI_SCAN_PATH "/sys/bus/platform/devices/gpio-fsi/fsi0/rescan"
+#define FSI_SCAN_PATH_OLD "/sys/devices/platform/fsi-master/scan"
+#define FSI_CFAM_PATH "/sys/devices/platform/gpio-fsi/fsi0/slave@00:00/raw"
+#define FSI_CFAM_PATH_OLD "/sys/devices/platform/fsi-master/slave@00:00/raw"
 #define FSI_SCOM_PATH "/sys/devices/platform/fsi-master/slave@00:00/"
 
 int fsi_fd;
 int scom_fd;
+static int fsi_old = 0;
+
+static uint32_t kernel_fsi_swap_endian(uint32_t data)
+{
+	return htobe32(data);
+}
+
+static uint32_t kernel_fsi_no_swap_endian(uint32_t data)
+{
+	return data;
+}
+
+static uint32_t (*swap_endian)(uint32_t) = kernel_fsi_swap_endian;
 
 static int kernel_putscom(struct target *target, uint64_t addr, uint64_t value)
 {
@@ -79,6 +96,7 @@ static int kernel_getscom(struct target *target, uint64_t addr, uint64_t *value)
 static int kernel_fsi_getcfam(struct target *target, uint64_t addr64, uint64_t *value)
 {
 	int rc;
+	uint32_t data;
 	uint32_t addr = (addr64 & 0x7ffc00) | ((addr64 & 0x3ff) << 2);
 
 	rc = lseek(fsi_fd, addr, SEEK_SET);
@@ -87,7 +105,7 @@ static int kernel_fsi_getcfam(struct target *target, uint64_t addr64, uint64_t *
 		return errno;
 	}
 
-	rc = read(fsi_fd, value, 4);
+	rc = read(fsi_fd, &data, 4);
 	if (rc < 0) {
 		if ((addr64 & 0xfff) != 0xc09)
 			/* We expect reads of 0xc09 to occasionally
@@ -97,12 +115,15 @@ static int kernel_fsi_getcfam(struct target *target, uint64_t addr64, uint64_t *
 		return errno;
 	}
 
+	*value = (uint64_t)swap_endian(data);
+
 	return 0;
 }
 
 static int kernel_fsi_putcfam(struct target *target, uint64_t addr64, uint64_t data)
 {
 	int rc;
+	uint32_t data32;
 	uint32_t addr = (addr64 & 0x7ffc00) | ((addr64 & 0x3ff) << 2);
 
 	rc = lseek(fsi_fd, addr, SEEK_SET);
@@ -111,7 +132,9 @@ static int kernel_fsi_putcfam(struct target *target, uint64_t addr64, uint64_t d
 		return errno;
 	}
 
-	rc = write(fsi_fd, &data, 4);
+	data32 = swap_endian((uint32_t)data);
+
+	rc = write(fsi_fd, &data32, 4);
 	if (rc < 0) {
 		warn("Failed to write to 0x%08x (%016llx)", addr, addr64);
 		return errno;
@@ -131,12 +154,20 @@ static void kernel_fsi_scan_devices(void)
 	int rc, fd;
 
 	fd = open(FSI_SCAN_PATH, O_WRONLY | O_SYNC);
-	if (fd < 0)
-		err(errno, "Unable to open %s", FSI_SCAN_PATH);
+	if (fd < 0) {
+		fd = open(FSI_SCAN_PATH_OLD, O_WRONLY | O_SYNC);
+		if (fd < 0)
+			err(errno, "Unable to open %s and %s", FSI_SCAN_PATH,
+			    FSI_SCAN_PATH_OLD);
+
+		swap_endian = kernel_fsi_no_swap_endian;
+		fsi_old = 1;
+	}
 
 	rc = write(fd, &one, sizeof(one));
 	if (rc < 0)
-		err(errno, "Unable to write to %s", FSI_SCAN_PATH);
+		err(errno, "Unable to write to %s",
+		    fsi_old ? FSI_SCAN_PATH_OLD : FSI_SCAN_PATH);
 
 	close(fd);
 }
@@ -147,11 +178,30 @@ int kernel_fsi_target_init(struct target *target, const char *name,
 	uint64_t value;
 
 	if (!fsi_fd) {
-		int tries = 5;
+		int tries = NUM_TRIES_PROBE;
 
 		while (tries) {
 			/* Open first raw device */
-			fsi_fd = open(FSI_CFAM_PATH, O_RDWR | O_SYNC);
+			if (fsi_old) 
+				fsi_fd = open(FSI_CFAM_PATH_OLD,
+					      O_RDWR | O_SYNC);
+			else {
+				fsi_fd = open(FSI_CFAM_PATH, O_RDWR | O_SYNC);
+				/* try old path if its the first try since
+				 * we haven't set fsi_old yet
+				 */
+				if (fsi_fd < 0 && tries == NUM_TRIES_PROBE) {
+					fsi_fd = open(FSI_CFAM_PATH_OLD,
+						      O_RDWR | O_SYNC);
+
+					if (fsi_fd >= 0) {
+						fsi_old = 1;
+						swap_endian = kernel_fsi_no_swap_endian;
+						goto found;
+					}
+				}
+			}
+
 			if (fsi_fd >= 0)
 				goto found;
 			tries--;
